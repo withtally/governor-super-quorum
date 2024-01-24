5dba57fa0c74b93506a80f6eb1467ce85aaac92e
Dennison Bertram <dennison@dennisonbertram.com>
2024-01-24 18:24:35 -0500

    fix:test for cloneing and init

--- test/Factory/Factory.test.ts
+++ test/Factory/Factory.test.ts
@@ -6,65 +6,105 @@ import { Factory, MyToken } from '../../typechain-types'; // Adjust the path acc
 
 
 describe('Factory Contract Tests', () => {
+
+    // Factory variables
     let factory: Factory;
     let signers: Signer[];
     let admin: Signer;
     let updater: Signer;
+    let mockAddress: Signer;
     let implementationAddress: string;
     let implementation2Address: string;
+    let encodedData: string;
+
+    //SuperQuorumGovernor Variables
+    let executionDelay = 0;
+    const votingDelay = 2;
+    const votingPeriod = 5; // 5 blocks
+    const extension = 0;
+    const quorumFraction = 10;
+    const superQuorumFraction = 60; // 60%
+    const proposalThreshold = 0;
+    const name = "MyGovernor"
 
     // Fixture to deploy the contract and set initial state
     async function deployFactoryFixture() {
         signers = await ethers.getSigners();
-        [admin, updater] = signers;
+        [admin, updater, mockAddress] = signers;
 
         const FactoryContract = await ethers.getContractFactory('Factory');
         factory = await FactoryContract.deploy() as Factory;
 
         // Deploy a mock implementation contract and set it in the factory
-        const ImplementationContract = await ethers.getContractFactory('MyToken');
-        const implementation = await ImplementationContract.deploy(admin.address);
-        const implementation2 = await ImplementationContract.deploy(admin.address);
+        const ImplementationContract = await ethers.getContractFactory('SuperQuorumGovernorUpgradeable');
+        const token_factory = await ethers.getContractFactory("MyToken");
+        const timelock_factory = await ethers.getContractFactory("TimelockUpgradeable")
+        const token = await token_factory.deploy(admin.address);
+        const timelock = await timelock_factory.deploy();
+        await timelock.initialize(0, [], [], admin.address);
+
+
+        const implementation = await ImplementationContract.deploy();
+        const implementation2 = await ImplementationContract.deploy();
 
         implementationAddress = await implementation.getAddress();
         implementation2Address = await implementation2.getAddress();
 
         await factory.initialize(implementationAddress, await updater.getAddress());
 
-        return { factory, implementation, implementation2, admin, updater };
+        encodedData = implementation.interface.encodeFunctionData("initialize",
+            [name, await token.getAddress(),
+                await timelock.getAddress(),
+                [votingDelay, extension],
+                votingPeriod,
+                proposalThreshold,
+                quorumFraction,
+                superQuorumFraction,
+            ])
+
+        return { factory, implementation, implementation2, admin, updater, encodedData };
     }
 
 
-    beforeEach(async () => {
-        const { factory, implementation, admin, updater, implementation2 } = await deployFactoryFixture();
-
-    });
-
     it('should deploy clone', async () => {
-        await expect(factory.connect(updater).updateImplementation(implementation2Address))
-            .to.emit(factory, 'ImplementationStored')
-            .withArgs(implementation2Address);
+        const { factory, admin, encodedData } = await deployFactoryFixture();
 
-        expect(await factory.implementation()).to.equal(implementation2Address);
+        // Deploy a clone
+        const tx = await factory.connect(admin).cloneAndInitialize(encodedData);
+        await tx.wait();
+
+        // Retrieve the event
+        const events = await factory.queryFilter(factory.filters['CloneCreated(address)']());
+
+        // Check if the event is present and has the correct argument
+        expect(events).to.not.be.empty;
+
+        const event = events[events.length - 1];
+        const clonedAddress = event.args.cloneAddress;
+
+        // Check if the clone is initialized
+        const clonedContract = await ethers.getContractAt('SuperQuorumGovernorUpgradeable', clonedAddress);
+        expect(await clonedContract.name()).to.equal(name);
+
     });
 
     it('should emit ImplementationStored event with correct address', async () => {
         const { factory, admin } = await deployFactoryFixture();
-    
+
         // Trigger the event
         const tx = await factory.connect(updater).updateImplementation(implementation2Address);
         await tx.wait();
-    
+
         // Retrieve the event
         const events = await factory.queryFilter(factory.filters.ImplementationStored());
-        
+
         // Check if the event is present and has the correct argument
         expect(events).to.not.be.empty;
         const event = events[events.length - 1];
         expect(event.args.implementation).to.equal(implementation2Address);
-      });
+    });
 
-    
+
     it('should update implementation address', async () => {
         const newImplementationAddress = "0x70ebAD30a31657A9cF7A748269C2FB0E63C2E4B7";
         await expect(factory.connect(updater).updateImplementation(newImplementationAddress))
